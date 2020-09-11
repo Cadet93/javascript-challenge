dcc02d6aede590b698fdf040d382bdec109684a0
kickboxerva <kickboxerva@yahoo.com>
2020-09-11 14:29:45 -0400

    updates.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -5,17 +5,17 @@ var tableData = data;
 var button = d3.select("#filter-btn");
 
 // Select the form
-var form = d3.select(".form-group");
+var form = d3.select("form");
 
 //variable for table body to add rows to
 var tbody = d3.select("tbody");
 
-// Select the input element and get the raw HTML node
-var inputElement = d3.select("#datetime");
-
 //event handlers
 button.on("click", runEnter);
-form.on("submit", runEnter);
+form.on("change", runEnter);
+//document.querySelector('.form-group').attachEvent('submit', runEnter);
+
+//d3.seletALL('.filter').on('change', runEnter);
 
 //Use d3 to update each cell's text with
 //Sighting values (date/time, city, state, country, shape, comment)
@@ -32,36 +32,70 @@ tableData.forEach(function (ufosightings) {
 });
 
 // Complete the event handler function for the form
-function runEnter() {
+function runEnter(event) {
 
-  // Prevent the page from refreshing
-  //d3.event.preventDefault();
-
-  // remove any children from the list to
   tbody.html("");
 
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var dateinput = d3.select("#datetime");
+  var cityinput = d3.select("#city");
+  var stateinput = d3.select("#state");
+  var countryinput = d3.select("#country");
+  var shapeinput = d3.select("#shape");
+
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var datevalue = dateinput.property("value");
+  var cityvalue = cityinput.property("value");
+  var statevalue = stateinput.property("value");
+  var countryvalue = countryinput.property("value");
+  var shapevalue = shapeinput.property("value");
 
-  console.log(inputValue);
 
-  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+  //create if statement
+  var filteredData = [...tableData]; //to make each filter independent of each other
+  if (datevalue) {
+    filteredData = filteredData.filter(obj => obj.datetime === datevalue);
+  }
+
+  if (cityvalue) {
+    filteredData = filteredData.filter(obj => obj.city.toLowerCase() === cityvalue.toLowerCase());
+  }
+
+  if (statevalue) {
+    filteredData = filteredData.filter(obj => obj.state.toLowerCase() === statevalue.toLowerCase());
+  }
+
+  if (countryvalue) {
+    filteredData = filteredData.filter(obj => obj.state.toLowerCase() === statevalue.toLowerCase());
+  }
+
+  if (shapevalue) {
+    filteredData = filteredData.filter(obj => obj.state.toLowerCase() === statevalue.toLowerCase());
+  }
 
   console.log(filteredData);
 
-  //Sighting values (date/time, city, state, country, shape, comment)
-  filteredData.forEach(function (filteredsightings) {
-    console.log(filteredsightings);
-    var row = tbody.append("tr");
-    Object.entries(filteredsightings).forEach(function ([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the ufosightings object
-      var cell = row.append("td");
-      cell.text(value);
+  if (filteredData.length >= 1) {
+    //Sighting values (date/time, city, state, country, shape, comment)
+    filteredData.forEach(function (filteredsightings) {
+      console.log(filteredsightings);
+      var row = tbody.append("tr");
+      Object.entries(filteredsightings).forEach(function ([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        // in the ufosightings object
+        var cell = row.append("td");
+        cell.text(value);
+      });
     });
-  });
+  }
+  else {
+    alert("No Results Found.")
 
+  }
 };
 
 
